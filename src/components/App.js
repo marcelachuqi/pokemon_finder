05c597578fa06c56c92fe109d5d945f3269723ed
Marcela Chuqi <81633985+marcelachuqi@users.noreply.github.com>
2021-08-04 21:07:26 +0200

    add  styles to list

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import PokeList from "./PokeList";
 import pokeData from "../data/pokeData.json";
 import "../stylesheets/style.css";
+import logo from "../image/538px-International_Pokémon_logo.svg.png";
 
 const App = () => {
   return (
     <>
-      <h1>Mi lista de Pokemones</h1>
+      <h1>
+        <img className="logo" src={logo} />
+      </h1>
       <PokeList list={pokeData} />
     </>
   );
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../stylesheets/style.css";
+import pokeball from "../image/pngwing.com.png";
 
 const Pokemon = (props) => {
   const renderTypes = () => {
@@ -9,21 +10,19 @@ const Pokemon = (props) => {
   };
 
   return (
-    <div className="divClass">
-      <ul>
-        {" "}
+    <div>
+      <img href="#" className="pokeball btn-neon" src={pokeball} />
+      <ul className="pokeList">
         <li>
-          <img className="pokeClass" src={props.src} />{" "}
-        </li>{" "}
+          <h1 className="pokeName"> {props.name} </h1>
+        </li>
+      </ul>{" "}
+      <img className="pokeImg" src={props.src} />
+      <ul className="pokeList">
+        <li></li>
       </ul>
-      <ul>
-        <li>
-          <h1 className="titleClass"> {props.name} </h1>{" "}
-        </li>{" "}
-      </ul>
-      <ul className="typeClass">
-        {" "}
-        <li>{renderTypes()} </li>{" "}
+      <ul className="pokeList">
+        <li className="pokeType">{renderTypes()} </li>
       </ul>
     </div>
   );
